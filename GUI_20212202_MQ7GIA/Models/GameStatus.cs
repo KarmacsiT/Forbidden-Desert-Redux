363f96882a3ecaeebec836c52c13d4f9e5a2fa6a
Karmacsi Tamás <karmacsitomi@gmail.com>
2022-04-09 15:09:46 +0200

    Created GameStatus in the Models folder

--- GUI_20212202_MQ7GIA/Models/GameStatus.cs
+++ GUI_20212202_MQ7GIA/Models/GameStatus.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace GUI_20212202_MQ7GIA.Models
 {
+    public enum Difficulty { Novice, Normal, Elite, Legendary }
     public class GameStatus
     {
-
+        public int NumberOfSandTiles { get; set; }
+        public Difficulty GameDifficulty { get; }
+        public int StormMeter { get; set; }
+        public int NumberOfStormCardsDrawn { get; set; }
     }
 }
