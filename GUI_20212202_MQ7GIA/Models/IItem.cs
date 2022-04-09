333803eb29916104ee7990648936baabe99ffd3c
Attila Kun <attil2000@vivaldi.net>
2022-04-09 14:22:34 +0200

    Created interfaces in models

---
+++ GUI_20212202_MQ7GIA/Models/IItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_20212202_MQ7GIA.Models
+{
+    public interface IItem // To be continued
+    {
+        string Name { get; set; }
+        void Ability(); 
+    }
+}
---
+++ GUI_20212202_MQ7GIA/Models/IRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_20212202_MQ7GIA.Models
+{
+    public interface IRole
+    {
+        string RoleName { get; set; }
+        int WaterLevel { get; set; }
+        string AbilityDescription { get; set; }
+        bool[] AbilityList { get; set; }
+        string ActionDescription { get; set; }
+    }
+}
