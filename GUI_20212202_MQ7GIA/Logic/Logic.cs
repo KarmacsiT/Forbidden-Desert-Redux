26cc5984b48d1fcfe49b8947cbe86c29e40354c3
Attila Kun <attil2000@vivaldi.net>
2022-04-13 19:07:41 +0200

    Created logic

--- GUI_20212202_MQ7GIA/Logic/Logic.cs
+++ GUI_20212202_MQ7GIA/Logic/Logic.cs
@@ -13,14 +13,40 @@ namespace GUI_20212202_MQ7GIA.Logic
         Deck deck;
         GameStatus status;
         Player player;
-        Storm storm;
+        ShipParts[] shipParts;
 
         public Logic()
         {
+            Random random = new Random();
             board = new Board
             {
-                Boar
+                TunnelTiles = new TunnelTile[3], // Okay, we have 3 tunnel tiles, not 2
+                AirShipClueTiles = new AirShipClueTile[8],
+                LaunchPadTile = new LaunchPadTile(),
+                OasisMirageTiles = new OasisMirageTile[3],
+                ShelterTiles = new ShelterTile[9],
+                storm = new Storm()
             };
+            board.storm.X = 2;
+            board.storm.Y = 2;
+            shipParts = new ShipParts[4];
+            shipParts[0] = new ShipParts { Name = "Crystal" };
+            shipParts[1] = new ShipParts { Name = "Engine" };
+            shipParts[2] = new ShipParts { Name = "Compass" };
+            shipParts[3] = new ShipParts { Name = "Propeller" };
+
+            //Parts generation
+            int shipPartCounter = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i != 2 && j != 2 && shipPartCounter < 4 && random.Next(0,100) < 50)
+                    {
+                        shipPartCounter++;
+                    }
+                }
+            }
         }
     }
 }
