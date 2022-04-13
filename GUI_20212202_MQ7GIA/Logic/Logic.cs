abcddd6e7bf3d6588dee3ff6ce32fbdaa7f4851f
Attila Kun <attil2000@vivaldi.net>
2022-04-13 19:55:55 +0200

    Created AirshipClueTile generation in Logic

--- GUI_20212202_MQ7GIA/Logic/Logic.cs
+++ GUI_20212202_MQ7GIA/Logic/Logic.cs
@@ -34,18 +34,38 @@ namespace GUI_20212202_MQ7GIA.Logic
             shipParts[1] = new ShipParts { Name = "Engine" };
             shipParts[2] = new ShipParts { Name = "Compass" };
             shipParts[3] = new ShipParts { Name = "Propeller" };
-
-            //Parts generation
-            int shipPartCounter = 0;
-            for (int i = 0; i < 5; i++)
+            int cardCounter = 0;
+            //We need this to avoid card generation conflicts when X = 0 and Y = 0
+            for (int i = 0; i < 8; i++)
             {
-                for (int j = 0; j < 5; j++)
+                board.AirShipClueTiles[i].X = -1;
+                board.AirShipClueTiles[i].Y = -1;
+            }
+            //AirShipClueTile generation
+            foreach (AirShipClueTile tile in board.AirShipClueTiles)
+            {
+                int X = random.Next(0, 4);
+                int Y = random.Next(0, 4);
+                //Avoid number generation for 2 and check if card is already generated
+                while (X != 2 && Y != 2 && (board.AirShipClueTiles.Any(coord => coord.X == X) && board.AirShipClueTiles.Any(coord => coord.Y == Y)))
                 {
-                    if (i != 2 && j != 2 && shipPartCounter < 4 && random.Next(0,100) < 50)
-                    {
-                        shipPartCounter++;
-                    }
+                    X = random.Next(0, 4);
+                    Y = random.Next(0, 4);
                 }
+                //Put direction
+                if (cardCounter % 2 == 0)
+                {
+                    tile.Direction = 'X';
+                }
+                else
+                {
+                    tile.Direction = 'Y';
+                }
+                if (cardCounter < 2) tile.PartName = shipParts[0].Name;
+                else if (cardCounter<4) tile.PartName = shipParts[1].Name;
+                else if (cardCounter<6) tile.PartName = shipParts[2].Name;
+                else tile.PartName = shipParts[3].Name;
+                cardCounter++;
             }
         }
     }
