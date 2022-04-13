d4dd2704da6a9a3ff77e9f20f830502d94687f77
Attila Kun <attil2000@vivaldi.net>
2022-04-13 18:40:44 +0200

    Created Logic class (initial)

---
+++ GUI_20212202_MQ7GIA/Logic/Logic.cs
@@ -0,0 +1,26 @@
+﻿using GUI_20212202_MQ7GIA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_20212202_MQ7GIA.Logic
+{
+    public class Logic
+    {
+        Board board;
+        Deck deck;
+        GameStatus status;
+        Player player;
+        Storm storm;
+
+        public Logic()
+        {
+            board = new Board
+            {
+                Boar
+            };
+        }
+    }
+}
