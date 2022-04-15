251d737a90c12b6eb8c0b5e0d1146217645b194b
Karmacsi Tamás <karmacsitomi@gmail.com>
2022-04-15 21:08:48 +0200

    GameLogic fixed

--- GUI_20212202_MQ7GIA/Logic/GameLogic.cs
+++ GUI_20212202_MQ7GIA/Logic/GameLogic.cs
@@ -14,11 +14,15 @@ namespace GUI_20212202_MQ7GIA.Logic
         GameStatus status;
         Player player;
         ShipParts[] shipParts;
-        public string[,] Names { get; set; }
+        public string[,] TileNames { get; set; }
+        public string[,] PartTiles { get; set; }
 
         Random random = new Random();
         public GameLogic()
         {
+            bool[,] isTaken = new bool[5, 5];
+            TileNames = new string[5, 5];
+
             board = new Board
             {
                 TunnelTiles = new TunnelTile[3], // Okay, we have 3 tunnel tiles, not 2
@@ -30,6 +34,9 @@ namespace GUI_20212202_MQ7GIA.Logic
             };
             board.storm.X = 2;
             board.storm.Y = 2;
+            TileNames[board.storm.X, board.storm.Y] = "Storm";
+            isTaken[board.storm.X, board.storm.Y] = true;
+
             shipParts = new ShipParts[4];
             shipParts[0] = new ShipParts { Name = "Crystal" };
             shipParts[1] = new ShipParts { Name = "Engine" };
@@ -45,8 +52,7 @@ namespace GUI_20212202_MQ7GIA.Logic
                 board.AirShipClueTiles[i].Y = -1;
             }
             //AirShipClueTile generation
-            bool[,] isTaken = new bool[5, 5];
-            Names = new string[5, 5];
+
 
             foreach (AirShipClueTile tile in board.AirShipClueTiles)
             {
@@ -54,15 +60,17 @@ namespace GUI_20212202_MQ7GIA.Logic
                 int Y = random.Next(0, 5);
 
                 //Avoid number generation for 2 and check if card is already generated
-                while (X is 2 && Y is 2 || isTaken[X, Y])
+                while (isTaken[X, Y])
                 {
                     X = random.Next(0, 5);
                     Y = random.Next(0, 5);
                 }
+
                 tile.X = X;
                 tile.Y = Y;
                 isTaken[X, Y] = true;
-                Names[X, Y] = "AirShipClueTile";
+                TileNames[X, Y] = "AirShipClueTile";
+
                 //Put direction
                 if (cardCounter % 2 == 0)
                 {
@@ -80,53 +88,53 @@ namespace GUI_20212202_MQ7GIA.Logic
             }
             cardCounter = 0;
 
+            PartTiles = new string[5, 5];
+
             foreach (ShipParts part in shipParts)
             {
                 part.X = board.AirShipClueTiles[cardCounter].X;
                 part.Y = board.AirShipClueTiles[cardCounter + 1].Y;
-                Names[part.X, part.Y] = board.AirShipClueTiles[cardCounter].PartName;
+                PartTiles[part.X, part.Y] = board.AirShipClueTiles[cardCounter].PartName;
                 cardCounter += 2;
             }
 
             // LaunchPadTile 
+            int[] coordinates = CoordinateGiver(isTaken);
             board.LaunchPadTile = new LaunchPadTile();
-            board.LaunchPadTile.X = CoordinateGiver(isTaken)[0];
-            board.LaunchPadTile.Y = CoordinateGiver(isTaken)[1];
-            Names[board.LaunchPadTile.X, board.LaunchPadTile.Y] = "LaunchPadTile";
+            board.LaunchPadTile.X = coordinates[0];
+            board.LaunchPadTile.Y = coordinates[1];
+            isTaken[board.LaunchPadTile.X, board.LaunchPadTile.Y] = true;
+            TileNames[board.LaunchPadTile.X, board.LaunchPadTile.Y] = "LaunchPadTile";
 
             // TunnelTiles && OasisMirageTiles
-            bool dry = false;
 
             for (int i = 0; i < 3; i++)
             {
                 board.TunnelTiles[i] = new TunnelTile();
-                board.TunnelTiles[i].X = CoordinateGiver(isTaken)[0];
-                board.TunnelTiles[i].Y = CoordinateGiver(isTaken)[1];
-                Names[board.TunnelTiles[i].X, board.TunnelTiles[i].Y] = "TunnelTile";
 
+                coordinates = CoordinateGiver(isTaken);
+                board.TunnelTiles[i].X = coordinates[0];
+                board.TunnelTiles[i].Y = coordinates[1];
+                isTaken[board.TunnelTiles[i].X, board.TunnelTiles[i].Y] = true;
+                TileNames[board.TunnelTiles[i].X, board.TunnelTiles[i].Y] = "TunnelTile";
+
+                coordinates = CoordinateGiver(isTaken);
                 board.OasisMirageTiles[i] = new OasisMirageTile();
-                board.OasisMirageTiles[i].X = CoordinateGiver(isTaken)[0];
-                board.OasisMirageTiles[i].Y = CoordinateGiver(isTaken)[1];
+                board.OasisMirageTiles[i].X = coordinates[0];
+                board.OasisMirageTiles[i].Y = coordinates[1];
+                isTaken[board.OasisMirageTiles[i].X, board.OasisMirageTiles[i].Y] = true;
 
-                if (dry == false)
+                if (i is 0)
                 {
-                    int chance = random.Next(0, 3);
-                    if (chance == 0)
-                    {
-                        board.OasisMirageTiles[i].IsDried = true;
-                    }
-                    dry = true;
+                    board.OasisMirageTiles[i].IsDried = true; //Generate Mirage
+                    TileNames[board.OasisMirageTiles[i].X, board.OasisMirageTiles[i].Y] = "Mirage";
                 }
+
                 else
                 {
-                    board.OasisMirageTiles[i].IsDried = false;
+                    board.OasisMirageTiles[i].IsDried = false; //Generate Oasis
+                    TileNames[board.OasisMirageTiles[i].X, board.OasisMirageTiles[i].Y] = "Oasis";
                 }
-
-                if (board.OasisMirageTiles[i].IsDried == false)
-                {
-                    Names[board.OasisMirageTiles[i].X, board.OasisMirageTiles[i].Y] = "Oasis";
-                }
-                else Names[board.OasisMirageTiles[i].X, board.OasisMirageTiles[i].Y] = "Mirage";
             }
 
             // ShelterTiles
@@ -137,40 +145,51 @@ namespace GUI_20212202_MQ7GIA.Logic
 
             foreach (ShelterTile tile in board.ShelterTiles)
             {
-                for (int x = 0; x < isTaken.GetLength(0); x++)
+                int x = 0;
+                bool breakLoops = false;
+
+                while (x < isTaken.GetLength(0) && !breakLoops)
                 {
-                    for (int y = 0; y < isTaken.GetLength(1); y++)
+                    int y = 0;
+
+                    while (y < isTaken.GetLength(1) && !breakLoops)
                     {
                         if (isTaken[x, y] is false)
                         {
                             tile.X = x;
                             tile.Y = y;
+                            isTaken[tile.X, tile.Y] = true;
+
+                            int chance = random.Next(0, 101);
+
+                            if (chance <= 50)
+                            {
+                                tile.ShelterType = ShelterVariations.Empty;
+                                TileNames[tile.X, tile.Y] = "EmptyShelter";
+                            }
+                            else if (chance > 50 && chance <= 65)
+                            {
+                                tile.ShelterType = ShelterVariations.FriendlyWater;
+                                TileNames[tile.X, tile.Y] = "FriendlyWater";
+                            }
+                            else if (chance > 65 && chance <= 80)
+                            {
+                                tile.ShelterType = ShelterVariations.FriendlyQuest;
+                                TileNames[tile.X, tile.Y] = "FriendlyQuest";
+                            }
+                            else
+                            {
+                                tile.ShelterType = ShelterVariations.Hostile;
+                                TileNames[tile.X, tile.Y] = "Hostile";
+                            }
+
+                            breakLoops = true;
                         }
+                        y++;
                     }
-                }
-                int chance = random.Next(0, 101);
-                if (chance <= 50)
-                {
-                    tile.ShelterType = ShelterVariations.Empty;
-                    Names[tile.X, tile.Y] = "EmptyShelter";
-                }
-                else if (chance > 50 && chance <= 65)
-                {
-                    tile.ShelterType = ShelterVariations.FriendlyWater;
-                    Names[tile.X, tile.Y] = "FriendlyWater";
-                }
-                else if (chance > 65 && chance <= 80)
-                {
-                    tile.ShelterType = ShelterVariations.FriendlyQuest;
-                    Names[tile.X, tile.Y] = "FriendlyQuest";
-                }
-                else
-                {
-                    tile.ShelterType = ShelterVariations.Hostile;
-                    Names[tile.X, tile.Y] = "Hostile";
+                    x++;
                 }
             }
-
         }
 
         private int[] CoordinateGiver(bool[,] isTaken)
@@ -178,12 +197,13 @@ namespace GUI_20212202_MQ7GIA.Logic
             int[] result = new int[2];   // 0--X , 1 ---Y
             int x = random.Next(0, 5);
             int y = random.Next(0, 5);
-            while (x is 2 && y is 2 && isTaken[x, y] is not false)  //The coordinate can't be the coordinate of the storm start coordinate which is [2,2] or taken
+
+            while (isTaken[x, y])  //The coordinate can't be the coordinate of the storm start coordinate which is [2,2] or taken
             {
                 x = random.Next(0, 5);
                 y = random.Next(0, 5);
             }
-            isTaken[x, y] = true;
+
             result[0] = x;
             result[1] = y;
             return result;
